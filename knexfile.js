cc57c054d48c78633aad28fee4504ef1e020934d
huong9x <huongnv@sphinx-software.com>
2019-04-16 11:22:28 +0700

    update config knex

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'mysql',
     connection: {
-      host : '127.0.0.1',
+      host : 'localhost',
       user : 'nodeApp',
       password : 'nodeApp',
       database : 'training',
