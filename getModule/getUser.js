9c1379d9dbacf0a6da4458c27382a337c709b020
huong9x <huongnv@sphinx-software.com>
2019-04-16 15:13:09 +0700

    update convention

--- getModule/getUser.js
+++ getModule/getUser.js
@@ -4,9 +4,9 @@ const configuration = require('../knexfile')['development'];
 const knex          = require('knex')(configuration); 
 let   getUser       = new Router();
 
-getUser.get('/search/:name',async (context,next) => {
-    let items    = await knex('users').where(knex.raw('?? like ?', ['users.user', '%'+context.params.name+'%']))
-    context.body = nunjucks.render('index.html',{items});
+getUser.get('/search/:name', async (context, next) => {
+    let items    = await knex('users').where(knex.raw('?? like ?', ['users.user', '%' + context.params.name + '%']))
+    context.body = nunjucks.render('index.html', { items });
 });
 
 module.exports   = getUser;
